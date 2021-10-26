2e79ff459fb03d186c42e8da00869ad6166c90dd
HM Tamim <hm.tamim@northsouth.edu>
2021-10-26 21:52:46 +0600

    Switch to grid to 2, 3, 4 columns done; App action bar UI added

--- app/src/main/java/com/hmtamim/imagesearch/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/hmtamim/imagesearch/ui/gallery/GalleryFragment.kt
@@ -1,12 +1,12 @@
 package com.hmtamim.imagesearch.ui.gallery
 
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
 import com.hmtamim.imagesearch.R
 import com.hmtamim.imagesearch.databinding.FragmentGalleryBinding
-import com.hmtamim.imagesearch.databinding.FragmentHomeBinding
 import com.hmtamim.imagesearch.ui.base.BaseFragment
 import com.hmtamim.imagesearch.ui.gallery.controller.GalleryController
-import com.hmtamim.imagesearch.utils.ToastUtils
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +15,7 @@ class GalleryFragment : BaseFragment<FragmentGalleryBinding, GalleryViewModel>(
     R.layout.fragment_gallery
 ) {
 
+    private lateinit var layoutManager: GridLayoutManager
     private val controller: GalleryController by lazy {
         GalleryController()
     }
@@ -28,9 +29,21 @@ class GalleryFragment : BaseFragment<FragmentGalleryBinding, GalleryViewModel>(
             controller.list = it
             controller.requestModelBuild()
         })
+
+        viewModel.getGridSizeLiveData().observe(viewLifecycleOwner, Observer {
+            layoutManager.spanCount = it
+            binding.btnGrid.setImageDrawable(
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    viewModel.getGridImageDrawable(it)
+                )
+            )
+        })
     }
 
     override fun setupRecycler() {
+        layoutManager = GridLayoutManager(context, 2)
+        binding.recyclerView.layoutManager = layoutManager
         binding.recyclerView.adapter = controller.adapter
     }
 
--- app/src/main/java/com/hmtamim/imagesearch/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/hmtamim/imagesearch/ui/gallery/GalleryViewModel.kt
@@ -3,6 +3,7 @@ package com.hmtamim.imagesearch.ui.gallery
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.hmtamim.imagesearch.R
 import com.hmtamim.imagesearch.data.repository.ApiRepository
 import com.hmtamim.imagesearch.data.room.entity.ImageEntity
 import com.hmtamim.imagesearch.model.PhotosResponse
@@ -15,9 +16,11 @@ class GalleryViewModel @Inject constructor(
     var apiRepository: ApiRepository
 ) : ViewModel() {
 
+    private var gridSizeLiveData: MutableLiveData<Int> = MutableLiveData()
     private val photosLiveList: MutableLiveData<List<ImageEntity>> = MutableLiveData()
 
     init {
+        gridSizeLiveData.value = 2
         getPhotos()
     }
 
@@ -30,12 +33,40 @@ class GalleryViewModel @Inject constructor(
             override fun onError(errorBody: String, errorCode: Int) {
 
             }
-
         })
     }
 
     fun getPhotosLiveData(): LiveData<List<ImageEntity>> {
         return photosLiveList
+    }
+
+    fun getGridSizeLiveData(): LiveData<Int> {
+        return gridSizeLiveData
+    }
+
+    fun updateGridSize() {
+        var currentGridSize = 2
+        var nextGridSize = 3
+        gridSizeLiveData.value?.let {
+            currentGridSize = it
+        }
+
+        when (currentGridSize) {
+            2 -> nextGridSize = 3
+            3 -> nextGridSize = 4
+            4 -> nextGridSize = 2
+        }
+
+        gridSizeLiveData.value = nextGridSize
+    }
+
+    fun getGridImageDrawable(currentGridSize: Int): Int {
+        return when (currentGridSize) {
+            2 -> R.drawable.ic_grid_3
+            3 -> R.drawable.ic_grid_4
+            4 -> R.drawable.ic_grid_2
+            else -> R.drawable.ic_grid_2
+        }
     }
 
 }
